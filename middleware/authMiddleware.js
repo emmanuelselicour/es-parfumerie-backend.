6d657af88dd3d52517a5afa4a64dcf78fb3e2601
emmanuelselicour <emmanuelselicour.2002@gmail.com>
2026-01-17 20:43:23 -0500

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ message: "Unauthorized" });
+
+  const token = authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+module.exports = authMiddleware;
