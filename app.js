a5e2f088632757309b8df654e3800c94b0392fbf
emmanuelselicour <emmanuelselicour.2002@gmail.com>
2026-01-17 20:45:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
@@ -10,4 +10,11 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 
-module.exports = app;
+app.get("/", (req, res) => {
+  res.send("ES Parfumerie Backend");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
